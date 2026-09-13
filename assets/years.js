cf5b87741afa2a451d54eec85a76a0d30e40a914
shukrullo <boburovdev@gmail.com>
2026-09-13 08:37:21 +0500

    Update note formatting for rendering line item in years.js

--- assets/years.js
+++ assets/years.js
@@ -372,7 +372,7 @@
         {icon:'cage',    label:'Ko‘p qavatli kataklarda tovuq boqish tizimi',note:'Tannarxni kamaytirish\n14.4 mlrd (1.2%)'},
         {icon:'pack',    label:'Parrada soyish , qayta ishlash , saqlash va qadoqlash', note:'Qo`shilgan qiymat - 915 mlrd Tannarxni kamaytrish - 36 mlrd (3%)'},
         {icon:'recycle', label:'Rendering liniyasi',
-         note:'Qo`shilgan qiymat - 49.7 mlrd so`m'}
+         note:'Qo`shilgan qiymat\n49.7 mlrd so`m'}
       ]
     }
   },
